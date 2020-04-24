09b79c76977bc929bdf1b50f168ba28f30b8baec
jasmaa <jasonmaa3955@gmail.com>
2020-04-24 09:25:48 -0400

    Improve position for miho

--- rendering.py
+++ rendering.py
@@ -5,7 +5,8 @@ import numpy as np
 shades_im = Image.open('shades.png')
 face_im = Image.open('miho_head.png')
 hair_im = Image.open('miho_hair.png')
-eye_im = Image.open('miho_eye.png')
+left_eye_im = Image.open('miho_left_eye.png')
+right_eye_im = Image.open('miho_right_eye.png')
 
 def transform_shades(input_im, frame_im, shape):
     """Transforms shades image
@@ -44,7 +45,7 @@ def transform_face(input_im, frame_im, shape):
         [0, input_im.size[1]],
     ])
 
-    u_diff = (0.8*(shape[16]-shape[0])).astype(int)
+    u_diff = (0.7*(shape[16]-shape[0])).astype(int)
     v_diff = (0.8*(shape[8]-shape[27])).astype(int)
 
     pb = np.float32([
@@ -66,7 +67,7 @@ def transform_hair(input_im, frame_im, shape):
         [0, input_im.size[1]],
     ])
 
-    u_diff = (1.2*(shape[16]-shape[0])).astype(int)
+    u_diff = (1.1*(shape[16]-shape[0])).astype(int)
     v_diff = (1.2*(shape[8]-shape[27])).astype(int)
 
     pb = np.float32([
@@ -89,11 +90,11 @@ def transform_left_eye(input_im, frame_im, shape):
     ])
 
     u_diff = (shape[45]-shape[42]).astype(int)
-    v_diff = (3*(shape[46]-shape[44])).astype(int)
+    v_diff = (4*(shape[46]-shape[44])).astype(int)
 
-    u_diff_1 = (0.3*(shape[16]-shape[0])).astype(int)
-    v_diff_1 = (0.1*(shape[8]-shape[27])).astype(int)
-    p_0 = u_diff_1 + v_diff_1
+    u_0 = (0.28*(shape[16]-shape[0])).astype(int)
+    v_0 = (0.12*(shape[8]-shape[27])).astype(int)
+    p_0 = u_0 + v_0
 
     pb = np.float32([
         shape[30] - u_diff - v_diff + p_0,
@@ -115,11 +116,11 @@ def transform_right_eye(input_im, frame_im, shape):
     ])
 
     u_diff = (shape[39]-shape[36]).astype(int)
-    v_diff = (3*(shape[41]-shape[37])).astype(int)
+    v_diff = (4*(shape[41]-shape[37])).astype(int)
 
-    u_diff_1 = (0.3*(shape[16]-shape[0])).astype(int)
-    v_diff_1 = (0.1*(shape[8]-shape[27])).astype(int)
-    p_0 = -u_diff_1 + v_diff_1
+    u_0 = (0.28*(shape[16]-shape[0])).astype(int)
+    v_0 = (0.12*(shape[8]-shape[27])).astype(int)
+    p_0 = -u_0 + v_0
 
     pb = np.float32([
         shape[30] - u_diff - v_diff + p_0,
@@ -139,8 +140,8 @@ def render(frame_im, shape):
     shades_im_mod = transform_shades(shades_im, frame_im, shape)
     face_im_mod = transform_face(face_im, frame_im, shape)
     hair_im_mod = transform_hair(hair_im, frame_im, shape)
-    left_eye_im_mod = transform_left_eye(eye_im, frame_im, shape)
-    right_eye_im_mod = transform_right_eye(eye_im.transpose(Image.FLIP_LEFT_RIGHT), frame_im, shape)
+    left_eye_im_mod = transform_left_eye(left_eye_im, frame_im, shape)
+    right_eye_im_mod = transform_right_eye(right_eye_im, frame_im, shape)
 
     # Paste
     frame_im.paste(face_im_mod, (0, 0), face_im_mod)
@@ -148,6 +149,6 @@ def render(frame_im, shape):
     frame_im.paste(left_eye_im_mod, (0, 0), left_eye_im_mod)
     frame_im.paste(right_eye_im_mod, (0, 0), right_eye_im_mod)
     
-    frame_im.paste(shades_im_mod, (0, 0), shades_im_mod)
+    #frame_im.paste(shades_im_mod, (0, 0), shades_im_mod)
 
     return frame_im
